c880de1c38b1f8df2f179f5e82cefa455a45c0ee
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2025-12-08 12:36:04 -0300

    adding fonts

--- styles/fonts.ts
+++ styles/fonts.ts
@@ -22,7 +22,83 @@ const mono = localFont({
   ],
 })
 
-const fonts = [mono]
+const geist = localFont({
+  src: [
+    {
+      path: '../public/fonts/Geist/Geist-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--next-font-geist',
+  preload: true,
+  adjustFontFallback: 'Arial',
+  fallback: [
+    'ui-monospace',
+    'SFMono-Regular',
+    'Consolas',
+    'Liberation Mono',
+    'Menlo',
+    'monospace',
+  ],
+})
+
+const geistMono = localFont({
+  src: [
+    {
+      path: '../public/fonts/GeistMono/GeistMono-Regular.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/GeistMono/GeistMono-Medium.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/GeistMono/GeistMono-Bold.woff2',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--next-font-geist-mono',
+  preload: true,
+  adjustFontFallback: 'Arial',
+  fallback: [
+    'ui-monospace',
+    'SFMono-Regular',
+    'Consolas',
+    'Liberation Mono',
+    'Menlo',
+    'monospace',
+  ],
+})
+
+const sentient = localFont({
+  src: [
+    {
+      path: '../public/fonts/Sentient/Sentient-Light.woff2',
+      weight: '300',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+  variable: '--next-font-sentient',
+  preload: true,
+  adjustFontFallback: 'Arial',
+  fallback: [
+    'ui-monospace',
+    'SFMono-Regular',
+    'Consolas',
+    'Liberation Mono',
+    'Menlo',
+    'monospace',
+  ],
+})
+
+const fonts = [mono, geist, geistMono, sentient]
 const fontsVariable = fonts.map((font) => font.variable).join(' ')
 
 export { fontsVariable }
