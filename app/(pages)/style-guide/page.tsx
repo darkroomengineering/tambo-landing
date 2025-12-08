01a8f15b7edc4c51ca0911eaa60a7c9bde846515
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2025-12-08 13:38:48 -0300

    colors variables

--- app/(pages)/style-guide/page.tsx
+++ app/(pages)/style-guide/page.tsx
@@ -3,7 +3,7 @@ import { Wrapper } from '../_components/wrapper'
 export default function StyleGuide() {
   return (
     <Wrapper theme="light" lenis={{}}>
-      <div className="dr-pt-120 px-safe flex flex-col dr-gap-15 h-screen">
+      <div className="dr-pt-120 px-safe flex flex-col dr-gap-15">
         <h1 className="h1">Heading 1</h1>
         <h2 className="h2">Heading 2</h2>
         <h3 className="h3">Heading 3</h3>
@@ -17,6 +17,66 @@ export default function StyleGuide() {
         <p className="button">Button</p>
         <p className="link">Link</p>
         <p className="surtitle">Surtitle</p>
+        <div className="dr-h-50 w-full bg-ghost-mint">
+          <p className="p">Ghost Mint</p>
+        </div>
+        <div className="dr-h-50 w-full bg-mint">
+          <p className="p">Mint</p>
+        </div>
+        <div className="dr-h-50 w-full bg-teal">
+          <p className="p">Teal</p>
+        </div>
+        <div className="dr-h-50 w-full bg-dark-teal">
+          <p className="p">Dark Teal</p>
+        </div>
+        <div className="dr-h-50 w-full bg-off-white">
+          <p className="p">Off White</p>
+        </div>
+        <div className="dr-h-50 w-full bg-light-gray">
+          <p className="p">Light Gray</p>
+        </div>
+        <div className="dr-h-50 w-full bg-grey">
+          <p className="p">Grey</p>
+        </div>
+        <div className="dr-h-50 w-full bg-dark-grey">
+          <p className="p">Dark Grey</p>
+        </div>
+        <div className="dr-h-50 w-full bg-white">
+          <p className="p">White</p>
+        </div>
+        <div className="dr-h-50 w-full bg-white-80">
+          <p className="p">White 80</p>
+        </div>
+        <div className="dr-h-50 w-full bg-white-60">
+          <p className="p">White 60</p>
+        </div>
+        <div className="dr-h-50 w-full bg-white-50">
+          <p className="p">White 50</p>
+        </div>
+        <div className="dr-h-50 w-full bg-black">
+          <p className="p text-white">Black</p>
+        </div>
+        <div className="dr-h-50 w-full bg-black-70">
+          <p className="p">Black 70</p>
+        </div>
+        <div className="dr-h-50 w-full bg-black-50">
+          <p className="p">Black 50</p>
+        </div>
+        <div className="dr-h-50 w-full bg-forest">
+          <p className="p">Forest</p>
+        </div>
+        <div className="dr-h-50 w-full bg-forest-50">
+          <p className="p">Forest 50</p>
+        </div>
+        <div className="dr-h-50 w-full bg-forest-30">
+          <p className="p">Forest 30</p>
+        </div>
+        <div className="dr-h-50 w-full bg-forest-20">
+          <p className="p">Forest 20</p>
+        </div>
+        <div className="dr-h-50 w-full bg-forest-10">
+          <p className="p">Forest 10</p>
+        </div>
       </div>
     </Wrapper>
   )
--- styles/colors.ts
+++ styles/colors.ts
@@ -1,11 +1,34 @@
 const colors = {
-  black: '#000000',
-  white: '#ffffff',
   red: '#e30613',
   blue: '#0070f3',
   green: '#00ff88',
   purple: '#7928ca',
   pink: '#ff0080',
+  // PRIMARY
+  'ghost-mint': '#D6FFEC',
+  mint: '#B6FFDD',
+  teal: '#7FFFC3',
+  'dark-teal': '#80C1A2',
+  // NEUTRAL
+  'off-white': '#F2F8F6',
+  'light-gray': '#E5F0ED',
+  grey: '#D8E9E4',
+  'dark-grey': '#CBE2DB',
+  // WHITE & SHADES
+  white: '#ffffff',
+  'white-80': '#ffffffcc',
+  'white-60': '#ffffff99',
+  'white-50': '#ffffff80',
+  // BLACK & SHADES
+  black: '#0F1A17',
+  'black-70': '#0f1a17b3',
+  'black-50': '#0f1a1780',
+  // FOREST & SHADES
+  forest: '#008346',
+  'forest-50': '#00834680',
+  'forest-30': '#0083464d',
+  'forest-20': '#00834633',
+  'forest-10': '#0083461a',
 } as const
 
 const themeNames = ['light', 'dark', 'red'] as const
