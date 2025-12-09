176e22e47b69e74599286e2e619c7ab76d1256f2
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2025-12-09 13:27:18 -0300

    section 3 wip

---
+++ app/(pages)/home/_sections/section-3/index.tsx
@@ -0,0 +1,7 @@
+export function Section3() {
+  return (
+    <section>
+      <h2>Section 3</h2>
+    </section>
+  )
+}
--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -2,6 +2,7 @@ import { Wrapper } from '../_components/wrapper'
 import Background from './_components/background'
 import { Section1 } from './_sections/section-1'
 import { Section2 } from './_sections/section-2'
+import { Section3 } from './_sections/section-3'
 
 export default function Home() {
   return (
@@ -16,6 +17,7 @@ export default function Home() {
       <Background>
         <Section1 />
         <Section2 />
+        <Section3 />
         <div className="h-[300vh]" />
       </Background>
     </Wrapper>
