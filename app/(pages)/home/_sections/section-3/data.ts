e4f6c3fff27cd3b92a1368bda2209af9182e3272
Fermin Fernandez <fermin.fernandez.bridda@gmail.com>
2025-12-09 15:34:56 -0300

    section 3 WIP

---
+++ app/(pages)/home/_sections/section-3/data.ts
@@ -0,0 +1,22 @@
+export const persons = [
+  {
+    name: 'Paul Copplestone',
+    account: '@kiwicopple',
+    image: '/assets/profiles/ProfilePic-1.png',
+    tweet: 'this is 🔥',
+  },
+  {
+    name: 'Thor 雷神 ⚡️',
+    account: '@thorwebdev',
+    image: '/assets/profiles/ProfilePic-2.png',
+    tweet:
+      'This is super cool 🤩 Streaming hyper personalised UI components into your app with @tambo_ai and @supabase MCP server!',
+  },
+  {
+    name: 'Seyam Alam',
+    account: '@thorwebdev',
+    image: '/assets/profiles/ProfilePic-3.png',
+    tweet:
+      "Thanks. Though wish I'd known about @tambo_ai sooner. Would have saved me at least a hundred hours in a separate project.",
+  },
+]
--- app/(pages)/home/_sections/section-3/index.tsx
+++ app/(pages)/home/_sections/section-3/index.tsx
@@ -1,7 +1,46 @@
+'use client'
+
+import { Image } from '~/components/image'
+import { Marquee } from '~/components/marquee'
+import { persons } from './data'
+
 export function Section3() {
   return (
     <section>
-      <h2>Section 3</h2>
+      <Marquee repeat={2} speed={0.3} className="dt:dr-mb-56">
+        <div className="flex dr-gap-x-24 dr-mr-24">
+          {persons.map((person) => (
+            <div
+              key={person?.name}
+              className="dt:dr-w-322 dt:dr-h-126 dt:dr-p-16 dt:dr-rounded-20 border border-dark-grey flex dr-gap-x-16"
+            >
+              <div className="dt:dr-w-40 dt:dr-h-40 dt:dr-rounded-full relative">
+                <Image src={person?.image} alt={person?.name} fill />
+              </div>
+              <div>
+                <div className="flex items-center dr-gap-x-4 w-full dr-mb-8">
+                  <p className="typo-p">{person?.name ?? ''}</p>
+                  <p className="typo-label-m text-black-50">
+                    {person?.account ?? ''}
+                  </p>
+                </div>
+                <div className="dt:dr-max-w-234 whitespace-normal">
+                  <p className="typo-p-s ">
+                    {(person?.tweet ?? '').split(' ').map((word, index) => (
+                      <span
+                        key={`${index}-${person?.name}`}
+                        className={word.startsWith('@') ? 'text-[#1D9BF0]' : ''}
+                      >
+                        {word}{' '}
+                      </span>
+                    ))}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </Marquee>
     </section>
   )
 }
