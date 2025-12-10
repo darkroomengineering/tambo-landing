6dce2e12a1b456a93e08b21df92be495f912417c
feledori <felix@felixmayr.it>
2025-12-10 17:37:43 +0100

    section 8

--- app/(pages)/home/_sections/section-8/index.tsx
+++ app/(pages)/home/_sections/section-8/index.tsx
@@ -1,9 +1,67 @@
+import cn from 'clsx'
+import { Fragment } from 'react'
+import PlaneSVG from '~/assets/svgs/plane.svg'
+import SpreadsheetSVG from '~/assets/svgs/spreadsheet.svg'
+import StocksSVG from '~/assets/svgs/stocks.svg'
+import { MCP } from './mcp'
+import s from './section-8.module.css'
+
 export function Section8() {
   return (
-    <section className="dr-layout-grid-inner h-screen">
-      <div className="col-start-2 col-end-12">
-        <h2 className="typo-h2">Section 8</h2>
+    <section className="flex flex-col dr-gap-20 items-center justify-center h-screen">
+      {/* TODO: Dashed border style*/}
+      <div className="dr-w-col-8 outline-off-white/80 outline-6 dr-rounded-20 aspect-898/597">
+        <div className="relative z-1 size-full dr-rounded-20 border border-forest/50 shadow-m bg-white">
+          <MCP />
+        </div>
+      </div>
+      <div className="relative z-1 dr-rounded-20 border border-dark-grey outline-6 outline-off-white/80 dr-w-col-8 dr-p-8 bg-white">
+        <div
+          className={cn(
+            s.tabs,
+            'h-full relative grid grid-flow-col dr-rounded-12 typo-h4 uppercase bg-off-white'
+          )}
+        >
+          {demos.map((demo) => (
+            <Fragment key={demo.id}>
+              <input
+                type="radio"
+                id={demo.id}
+                name="demo"
+                value={demo.id}
+                defaultChecked={demo.id === 'demo-1'}
+              />
+              <label
+                htmlFor={demo.id}
+                className="flex dr-gap-16 items-center justify-center dr-h-60"
+              >
+                <div className="dr-size-32 grid place-items-center rounded-full bg-mint">
+                  <demo.icon className="dr-size-16 icon text-black" />
+                </div>
+                <span className="block">{demo.label}</span>
+              </label>
+            </Fragment>
+          ))}
+        </div>
       </div>
     </section>
   )
 }
+
+const demos = [
+  {
+    id: 'demo-1',
+    label: 'travel assistant',
+    icon: PlaneSVG,
+  },
+  {
+    id: 'demo-2',
+    label: 'stocks dashboard',
+    icon: StocksSVG,
+  },
+  {
+    id: 'demo-3',
+    label: 'AI spreadsheet',
+    icon: SpreadsheetSVG,
+  },
+]
---
+++ app/(pages)/home/_sections/section-8/mcp.tsx
@@ -0,0 +1,3 @@
+export function MCP() {
+  return <div className="size-full" />
+}
