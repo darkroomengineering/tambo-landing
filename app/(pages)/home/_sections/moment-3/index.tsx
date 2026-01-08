cf27eb73998c25a3952ff157a73ffc4841b02bf7
feledori <felix@felixmayr.it>
2026-01-08 17:19:53 +0100

    add moment 2 proxy object

--- app/(pages)/home/_sections/moment-3/index.tsx
+++ app/(pages)/home/_sections/moment-3/index.tsx
@@ -1,15 +1,24 @@
 'use client'
+import { useRect } from 'hamo'
 import { TimelineSection } from '~/app/(pages)/home/_components/timeline-section'
 import { Animation } from './animation'
 import { messages } from './data'
 
 export function Moment3() {
+  const [setRectRef, rect] = useRect()
+
   return (
     <TimelineSection
       messages={messages}
       title="Native MCP support, hard wiring done for you."
       zIndex={3}
       id="moment-3"
+      proxyChildren={
+        <div
+          ref={setRectRef}
+          className="pointer-events-none dr-w-668 opacity-0 aspect-668/470"
+        />
+      }
     >
       <Animation />
     </TimelineSection>
